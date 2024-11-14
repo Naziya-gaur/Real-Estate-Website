70e503164aa2504c8b1947a803f32c78816ff68c
Naziya <130036243+Naziya-gaur@users.noreply.github.com>
2024-11-14 23:10:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('projectsButton').addEventListener('click', function() {
+    window.location.href = '/projects';  // Redirects to the projects page
+});
+
+document.getElementById('contactButton').addEventListener('click', function() {
+    window.location.href = '/contact';  // Redirects to the contact page
+});
+
